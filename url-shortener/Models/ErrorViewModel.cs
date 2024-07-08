1b39a30507841e421e5edec0787510390c20fc71
Son Anh <buisonanh.study@gmail.com>
2024-07-08 20:51:48 +0700

    Init

--- url-shortener/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace url_shortener.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
---
+++ url-shortener/gateway/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace gateway.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
